f04e25e06813124c7856d1568c92cf4d86606bd1
Alexander Entinger <consulting@lxrobotics.com>
2022-11-25 11:04:56 +0100

    Defining enum classes for ControlTable and OperatingMode.

---
+++ include/l3xz_head_ctrl/MX28AR/MX28AR_Const.h
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2022 LXRobotics GmbH.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/l3xz_ctrl/graphs/contributors.
+ */
+
+#ifndef L3XZ_MX28AR_CONST_H_
+#define L3XZ_MX28AR_CONST_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <cstdint>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace l3xz::mx28ar
+{
+
+/**************************************************************************************
+ * TYPEDEF
+ **************************************************************************************/
+
+enum class ControlTable : uint8_t
+{
+  OperatingMode = 11,
+};
+
+enum class OperatingMode : uint8_t
+{
+  VelocityControlMode         = 1,
+  PositionControlMode         = 3,
+  ExtendedPositionControlMode = 4,
+  PwmControlMode              = 16,
+};
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* l3xz::mx28ar */
+
+#endif /* L3XZ_MX28AR_CONST_H_ */
