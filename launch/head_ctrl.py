88c5f4254086f3987049cf5666bd7ccc8fc8bf28
Alexander Entinger <consulting@lxrobotics.com>
2023-02-23 09:14:30 +0100

    Specify both inital angles, as well as the correct topic remappings to fit with the ros2_dynamixe_bridge generated topics.

--- launch/head_ctrl.py
+++ launch/head_ctrl.py
@@ -11,6 +11,22 @@ def generate_launch_description():
       output='screen',
       emulate_tty=True,
       parameters=[
+        {'pan_initial_angle_deg': 180.0},
+        {'pan_min_angle_deg': 160.0},
+        {'pan_max_angle_deg': 200.0},
+        {'tilt_initial_angle_deg': 180.0},
+        {'tilt_min_angle_deg': 160.0},
+        {'tilt_max_angle_deg': 200.0},
+      ],
+      remappings=[
+        ('/dynamixel/servo_7/angle/actual',            '/l3xz/head/pan/angle/actual'),
+        ('/dynamixel/servo_7/angle/target',            '/l3xz/head/pan/angle/target'),
+        ('/dynamixel/servo_7/angular_velocity/target', '/l3xz/head/pan/angular_velocity/target'),
+        ('/dynamixel/servo_7/mode/set',                '/l3xz/head/pan/mode/set'),
+        ('/dynamixel/servo_8/angle/actual',            '/l3xz/head/tilt/angle/actual'),
+        ('/dynamixel/servo_8/angle/target',            '/l3xz/head/tilt/angle/target'),
+        ('/dynamixel/servo_8/angular_velocity/target', '/l3xz/head/tilt/angular_velocity/target'),
+        ('/dynamixel/servo_8/mode/set',                '/l3xz/head/tilt/mode/set'),
       ]
     )
   ])
